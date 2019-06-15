d48611a4eab22b4e138052308cfe6c56e1ee3456
John Chabot <johnchabot2013@gmail.com>
2019-06-15 10:00:54 -0400

    updated lang-server need help activating it From initial testing this current commit can properly activate the language server if it is manually activated. This does not appear to activate the atom ui elements to receive the server updates.

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,12 +1,11 @@
-import { TextEditor } from 'atom';
 import { AutoLanguageClient, ConnectionType, LanguageServerProcess } from 'atom-languageclient';
-export declare class KosLanguageClient extends AutoLanguageClient {
+declare class KosLanguageClient extends AutoLanguageClient {
     getGrammarScopes(): string[];
     getLanguageName(): string;
     getServerName(): string;
     getConnectionType(): ConnectionType;
     startServerProcess(): LanguageServerProcess;
-    shouldStartForEditor(editor: TextEditor): boolean;
-    validateKosServerPath(): boolean;
-    openPackageSettings(): void;
+    shouldStartForEditor(_: any): boolean;
 }
+export declare const client: KosLanguageClient;
+export {};
--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,41 @@
-import { TextEditor } from 'atom';
 import { AutoLanguageClient, ConnectionType, LanguageServerProcess } from 'atom-languageclient';
-import { isAbsolute, join } from 'path';
-import { existsSync } from 'fs';
+import { parse } from 'semver';
 
-atom.config.set('core.debugLSP', true)
+const args = ['--node-ipc'];
 
-export class KosLanguageClient extends AutoLanguageClient {
-  getGrammarScopes(): string[] { return ['source.kos']; }
-  getLanguageName(): string { return 'Kerbal Operating System'; }
-  getServerName(): string { return 'kos-language-server'; }
-  getConnectionType(): ConnectionType { return 'ipc'; }
+const { version } = process;
+const semver = parse(version);
+
+if (semver != null && semver.major < 10) {
+  args.unshift('--harmony_async_iteration')
+}
+
+class KosLanguageClient extends AutoLanguageClient {
+  getGrammarScopes(): string[] {
+    return ['source.kos'];
+  }
+  getLanguageName(): string {
+    return 'Kerbal Operating System';
+  }
+  getServerName(): string {
+    return 'kos-language-server';
+  }
+  getConnectionType(): ConnectionType {
+    return 'ipc';
+  }
 
   startServerProcess(): LanguageServerProcess {
-    console.log("start server process");
+    const server = require.resolve('kos-language-server/dist/server');
 
     return super.spawnChildNode(
-      [atom.config.get('language-kos.kosServer.path'), '--node-ipc'],
+      [server, ...args],
       { stdio: [null, null, null, 'ipc'] },
     ) as LanguageServerProcess;
   }
 
-  shouldStartForEditor(editor: TextEditor) {
-    console.log("should start editor");
-
-    if (!this.validateKosServerPath()) return false;
-    return super.shouldStartForEditor(editor);
-  }
-
-  validateKosServerPath(): boolean {
-    console.log("validate kos server path");
-
-    const kosSpecifiedPath = atom.config.get('language-kos.kosServer.path');
-    const isAbsolutelySpecified = isAbsolute(kosSpecifiedPath);
-    const kosAbsolutePath = isAbsolutelySpecified
-      ? kosSpecifiedPath
-      : join(__dirname, '..', kosSpecifiedPath);
-
-    if (existsSync(kosAbsolutePath)) return true;
-
-    atom.notifications.addError('language-kos could not locate the kos-language-server', {
-      dismissable: true,
-      buttons: [
-        { text: 'Set KOS server path', onDidClick: () => this.openPackageSettings() },
-      ],
-      description:
-        `No KOS server could be found at <b>${kosAbsolutePath}</b>`,
-    });
-
-    return false;
-  }
-
-  openPackageSettings() {
-    console.log("open package settings");
-
-    atom.workspace.open('atom://config/packages/language-kos');
+  shouldStartForEditor(_: any) {
+    return true;
   }
 }
+
+export const client = new KosLanguageClient();
